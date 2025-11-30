a054839f9cf75318e087e22c8f4db009d8921ab3
dramaticamo <dramaticamo1122002@gmail.com>
2025-11-30 02:04:22 +0000

    update: added responsive pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import { FavouritesContext } from "./context/FavouritesContext";
 
