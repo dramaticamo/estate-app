754f006d7585b6b615ae6ce55a77c4aada26f6e9
dramaticamo <dramaticamo1122002@gmail.com>
2025-11-30 19:39:53 +0000

    update: fixed images

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/estate-app/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/estate-app/" : "/", 
   plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./setupTests.js",
   },
-});
+}));
